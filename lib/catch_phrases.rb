bb93126f008d457128df0256b6af12ec5a97d9fa
masterful-array-3850 <crgammage@gmail.com>
2020-04-21 19:47:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,9 +13,6 @@ def link
   puts link
 end
 
-def all_phrases(all_phrases)
-phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  link = "It's Dangerous To Go Alone! Take This."
-  puts "#{all_phrases}"
+def all_phrases
+  
 end
