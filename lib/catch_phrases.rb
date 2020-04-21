74b8587b17c5243c94806e4e1de48d9217f213b3
masterful-array-3850 <crgammage@gmail.com>
2020-04-21 19:47:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,4 +17,5 @@ def all_phrases
   phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   link = "It's Dangerous To Go Alone! Take This."
+  puts 
 end
